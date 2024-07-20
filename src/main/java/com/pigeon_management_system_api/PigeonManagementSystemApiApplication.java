df37d8ddc75d42bc9c87e65327546455e81a1bb0
Mateusz <mateusz.blach@interia.pl>
2024-07-20 20:41:22 +0200

    initial commit

---
+++ src/main/java/com/pigeon_management_system_api/PigeonManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.pigeon_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigeon_management_system_api/PigeonManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigeon_management_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
