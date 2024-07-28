42b3a6bbb332f451715363a817d2f5fc08d17a8b
Mateusz <mateusz.blach@interia.pl>
2024-07-28 12:54:36 +0200

    add registration

--- src/main/java/com/pigeon_management_system_api/controller/UserController.java
+++ src/main/java/com/pigeon_management_system_api/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.pigeon_management_system_api.controller;
 
+import com.pigeon_management_system_api.dto.UserRegistrationDTO;
 import com.pigeon_management_system_api.model.User;
 import com.pigeon_management_system_api.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,16 +16,34 @@ import java.util.List;
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/all")
     public List<User> getAllUsers() {
-
         logger.info("Received request to get all users");
         List<User> users = userRepository.findAll();
-        logger.info("Found "+users.size()+" users");
+        logger.info("Found " + users.size() + " users");
 
         return users;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
+        logger.info("Received request to register user with email: " + userRegistrationDTO.getEmail());
+
+
+        User user = new User();
+        user.setName(userRegistrationDTO.getName());
+        user.setSurname(userRegistrationDTO.getSurname());
+        user.setEmail(userRegistrationDTO.getEmail());
+        user.setPassword(userRegistrationDTO.getPassword());
+        user.setBirthDate(userRegistrationDTO.getBirthDate());
+
+        User savedUser = userRepository.save(user);
+        logger.info("User registered with ID: " + savedUser.getId());
+
+        return savedUser;
     }
 }
--- src/main/java/com/pigeon_management_system_api/model/User.java
+++ src/main/java/com/pigeon_management_system_api/model/User.java
@@ -1,6 +1,5 @@
 package com.pigeon_management_system_api.model;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,18 +10,25 @@ import java.util.Date;
 @Entity
 @Table(name = "\"user\"")
 public class User implements Serializable {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name = "user_seq_gen", sequenceName = "user_seq", allocationSize = 1)
     @Column(name = "id")
     private Integer id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "surname")
     private String surname;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "password")
     private String password;
+
     @Column(name = "birth_date")
     private Date birthDate;
 }
