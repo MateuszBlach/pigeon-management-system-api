13ff7be0c30f5492fb8626946089973ef721e654
Mateusz <mateusz.blach@interia.pl>
2024-08-05 23:08:58 +0200

    refactor - move logic to service

--- src/main/java/com/pigeon_management_system_api/controller/UserController.java
+++ src/main/java/com/pigeon_management_system_api/controller/UserController.java
@@ -3,7 +3,6 @@ package com.pigeon_management_system_api.controller;
 import com.pigeon_management_system_api.dto.UserLoginDTO;
 import com.pigeon_management_system_api.dto.UserRegistrationDTO;
 import com.pigeon_management_system_api.model.User;
-import com.pigeon_management_system_api.repository.UserRepository;
 import com.pigeon_management_system_api.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,36 +21,23 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
     private UserService userService;
 
     @GetMapping("/all")
-    public List<User> getAllUsers() {
+    public ResponseEntity<List<User>> getAllUsers() {
         logger.info("Received request to get all users");
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.getAllUsers();
         logger.info("Found " + users.size() + " users");
 
-        return users;
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping("/register")
-    public User registerUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
+    public ResponseEntity<User> registerUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
         logger.info("Received request to register user with email: " + userRegistrationDTO.getEmail());
-
-
-        User user = new User();
-        user.setName(userRegistrationDTO.getName());
-        user.setSurname(userRegistrationDTO.getSurname());
-        user.setEmail(userRegistrationDTO.getEmail());
-        user.setPassword(userRegistrationDTO.getPassword());
-        user.setBirthDate(userRegistrationDTO.getBirthDate());
-
-        User savedUser = userRepository.save(user);
-        logger.info("User registered with ID: " + savedUser.getId());
-
-        return savedUser;
+        User registredUser = userService.registerNewUser(userRegistrationDTO);
+        logger.info("User registered with ID: " + registredUser.getId());
+        return ResponseEntity.ok(registredUser);
     }
 
     @PostMapping("/login")
--- src/main/java/com/pigeon_management_system_api/services/UserService.java
+++ src/main/java/com/pigeon_management_system_api/services/UserService.java
@@ -1,11 +1,13 @@
 package com.pigeon_management_system_api.services;
 
 import com.pigeon_management_system_api.dto.UserLoginDTO;
+import com.pigeon_management_system_api.dto.UserRegistrationDTO;
 import com.pigeon_management_system_api.model.User;
 import com.pigeon_management_system_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,5 +27,19 @@ public class UserService {
         }
 
         return Optional.empty();
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User registerNewUser(UserRegistrationDTO userRegistrationDTO){
+        User user = new User();
+        user.setName(userRegistrationDTO.getName());
+        user.setSurname(userRegistrationDTO.getSurname());
+        user.setEmail(userRegistrationDTO.getEmail());
+        user.setPassword(userRegistrationDTO.getPassword());
+        user.setBirthDate(userRegistrationDTO.getBirthDate());
+        return userRepository.save(user);
     }
 }
